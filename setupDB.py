55b1ed2d43aafb4a3b0ff283df86bcb0c9a1fdcb
green <green@cganalytics.org>
2015-05-01 22:35:14 -0700

    initialize database using database module

--- setupDB.py
+++ setupDB.py
@@ -1,30 +1,9 @@
 from database import Database
 
-#import psycopg2
-
-# Connect to an existing database
-#conn = psycopg2.connect(database="books")
-
-# Open a cursor to perform database operations
-#cur = conn.cursor()
-
 db = Database()
 db.connect()
 
-# Execute commands
-#db.cur.execute("CREATE TABLE users (username varchar, password varchar);")
-
 db.cur.execute("DROP TABLE books;")
 db.cur.execute("CREATE TABLE books (id serial PRIMARY KEY, username varchar, title varchar, type varchar, rating decimal, author varchar, numpages int, yearpub int, yearread int);")
-
-#db.cur.execute("DROP TABLE books;")
-#db.cur.execute("CREATE TABLE books (username varchar, title varchar, type varchar, rating varchar, author varchar, numpages varchar, yearpub varchar, yearread varchar);")
-
-# Make the changes to the database persistent
-#conn.commit()
-
-# Close communication with the database
-#cur.close()
-#conn.close()
 
 db.disconnect()
